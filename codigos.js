97c80f35b00fe8240de3f7417bf2da2a1c7ade27
Bruno Vieira <130676261+BrunoVieira9@users.noreply.github.com>
2023-05-30 16:12:38 -0300

    Desafio_Js02

---
+++ codigos.js
@@ -0,0 +1,19 @@
+
+
+function ativarNome(){
+    let nome = 'Bruno lindo';
+    console.log(nome)
+
+    document.getElementById('aparecer').innerHTML = nome
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+    let c = a*b;
+    console.log(c)
+
+    document.getElementById('aparecer').innerHTML = c
+
+}
